8949f597fdc282da05b1a42676555ca23508bae3
Shantanu Deshpande <shantanuudaydeshpande@gmail.com>
2015-06-04 17:36:29 -0400

    Contains Duplicate II

---
+++ 219.Contains-Duplicate-II/Solution.java
@@ -0,0 +1,15 @@
+public class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        HashMap<Integer,Integer> map = new HashMap<Integer,Integer>();
+        for (int i = 0; i < nums.length; ++i){
+            if(map.containsKey(nums[i])){
+                int j = map.get(nums[i]);
+                if (Math.abs(j - i) <= k){
+                    return true;
+                }
+            }
+            map.put(nums[i],i);
+        }
+        return false;
+    }
+}
